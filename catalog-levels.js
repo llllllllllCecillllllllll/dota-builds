656b7414c7a1ed5e3d0706a311857a9054f074ac
llllllllllCecillllllllll <chernomaziyy@gmail.com>
2026-08-26 17:32:11 +0500

    Add Dagon II-V and Boots of Travel II variants

--- catalog-levels.js
+++ catalog-levels.js
@@ -1,8 +1,15 @@
 (function(){'use strict';
-  var LEVELS=['Dagon 2','Dagon 3','Dagon 4','Dagon 5'];
+  var LEVELS=[
+    {name:'Dagon 2',categories:['Магия']},
+    {name:'Dagon 3',categories:['Магия']},
+    {name:'Dagon 4',categories:['Магия']},
+    {name:'Dagon 5',categories:['Магия']},
+    {name:'Boots of Travel 2',categories:['Поддержка']}
+  ];
   function esc(s){return String(s).replace(/[&<>\"']/g,function(c){return {'&':'&amp;','<':'&lt;','>':'&gt;','\"':'&quot;',"'":'&#039;'}[c];});}
-  function levelCards(){
-    return LEVELS.map(function(name){
+  function levelCards(category){
+    return LEVELS.filter(function(item){return category==='Все'||item.categories.indexOf(category)!==-1;}).map(function(item){
+      var name=item.name;
       return '<button class="item-choice" data-item="'+esc(name)+'" draggable="true">'+imageTag(name)+'<span class="item-name">'+esc(name)+'</span></button>';
     }).join('');
   }
@@ -13,9 +20,8 @@
       var html=base.apply(this,arguments);
       var cat=document.querySelector('.categories .cat.active');
       var category=cat?cat.dataset.category:'Все';
-      if(category==='Магия') html += levelCards();
-      if(category==='Все') html += levelCards();
-      return html;
+      var extra=levelCards(category);
+      return html+extra;
     };
     window.__dotaLevelsPatched=true;
   }
